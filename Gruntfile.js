80247a3235afabf9eec9f7ad0c2e386e82b865e0
Luciana <luciana342010@hotmail.com>
2024-08-19 17:08:25 -0300

    subindo exercicio

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,27 @@
 module.exports = function (grunt) {
-    // Carrega as configurações do package.json
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
         less: {
             development: {
-                options: {
-                    // Adicione aqui opções personalizadas para o desenvolvimento, se necessário
-                },
+                options: {},
                 files: {
-                    'dev/styles/main.css': 'src/styles/main.less'
+                    'css/style.css': 'src/styles/main.less'
                 }
-            },
-            production: {
-                options: {
-                    compress: true
-                },
+            }
+        },
+
+        uglify: {
+            my_target: {
                 files: {
-                    'dist/styles/main.min.css': 'src/styles/main.less'
+                    'js/scripts.min.js': 'src/scripts/main.js'
                 }
             }
         }
     });
 
-    // Carrega o plugin Grunt para compilar LESS
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    // Registra as tarefas
-    grunt.registerTask('default', ['less:development']);
-    grunt.registerTask('build', ['less:production']);
+    grunt.registerTask('default', ['less', 'uglify']);
 };
